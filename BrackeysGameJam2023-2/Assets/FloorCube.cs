336865bc07d5331f619f6827e0e3324785b756bb
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2023-08-26 13:55:57 +1200

    FloorCubes reduce size over time

--- BrackeysGameJam2023-2/Assets/FloorCube.cs
+++ BrackeysGameJam2023-2/Assets/FloorCube.cs
@@ -6,12 +6,30 @@ public class FloorCube : MonoBehaviour
 {
     public float despawnDuration;
     public float breakForce;
+    public AnimationCurve sizeCurve;
 
     public void Break()
     {
         Rigidbody rb = gameObject.AddComponent<Rigidbody>();
         rb.AddForce(Vector3.down * breakForce);
-        Destroy(gameObject, despawnDuration);
+        StartCoroutine(Despawn());
+    }
+
+    IEnumerator Despawn()
+    {
+        float t = despawnDuration;
+        Vector3 localScale = transform.localScale;
+        
+        //Shrink over time.
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            transform.localScale = localScale * sizeCurve.Evaluate(t);
+
+            yield return null;
+        }
+        Destroy(gameObject);
+        yield return null;
     }
 
 
