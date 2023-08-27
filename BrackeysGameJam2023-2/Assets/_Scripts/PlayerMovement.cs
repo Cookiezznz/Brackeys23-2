ee9846c9726e585616618c98eb808c7b5e2f10e7
Dubsomni <dubsomni@gmail.com>
2023-08-27 16:11:20 +1200

    Sprite animation update

--- BrackeysGameJam2023-2/Assets/_Scripts/PlayerMovement.cs
+++ BrackeysGameJam2023-2/Assets/_Scripts/PlayerMovement.cs
@@ -19,6 +19,8 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 moveDirection;
     public Vector2 xConstraints;
     public Vector2 zConstraints;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     public float speedReducedPerNearbyHostile;
     public AnimationCurve attackHoldSlowOverDuration;
@@ -46,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
         {
             //Reset accelleration if player has stopped moving
             if (moveAccelleration > 0) moveAccelleration = 0;
+            animator.SetBool("isRunning", false);
             return;
         }
 
@@ -56,7 +59,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 translatePosition = speed * moveDirection;
         currentSpeed = translatePosition;
         transform.Translate(translatePosition * Time.deltaTime);
-        
+        if (moveDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        animator.SetBool("isRunning", false);
+
         //Increment moveAccelleration if needed
         if (moveAccelleration < moveAccellerationDuration)
             moveAccelleration = Mathf.Min(moveAccelleration += Time.deltaTime, moveAccellerationDuration);
